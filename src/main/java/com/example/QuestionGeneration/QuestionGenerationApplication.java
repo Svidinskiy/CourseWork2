96fd100070d7595397481747a60a1a82cffa601e
Svidinskiy <100801664+Svidinskiy@users.noreply.github.com>
2023-07-12 15:39:45 +0400

    Первый коммит. Курсовая работа 2

---
+++ src/main/java/com/example/QuestionGeneration/QuestionGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuestionGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionGenerationApplication.class, args);
+	}
+
+}
